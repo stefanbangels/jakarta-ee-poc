d4132e85a2a55b56943523845c0ba6b87a78eabe
Stefan Bangels <stefan.bangels@gmail.com>
2019-10-13 19:16:54 +0200

    fix build

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-gradlew clean build
-docker rm -f jakarta-ee-poc || true && docker run -d -p 8080:8080 -p 9990:9990 --name jakarta-ee-poc org.assertdevelopments/jakarta-ee-poc
+./gradlew clean build
+docker build -t jakarta-ee-poc .
+docker rm -f jakarta-ee-poc
+docker run -d -p 8080:8080 -p 9990:9990 --name jakarta-ee-poc jakarta-ee-poc
